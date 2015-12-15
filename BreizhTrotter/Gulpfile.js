c43a785cda46ace2bc32242af9cfa3ade734915a
jerome.auffret <jerome.auffret.ja@gmail.com>
2015-12-15 14:49:29 +0100

    Installation de Gulp

---
+++ BreizhTrotter/Gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+var sass = sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('./web/bundles/AppBundle/sass/master.scss')
+        .pipe(sass({sourceComments: 'map'}))
+        .pipe(gulp.dest('./web/css/'));
+});
+
+gulp.task('watch', function () {
+    var onChange = function (event) {
+        console.log('File '+event.path+' has been '+event.type);
+    };
+    gulp.watch('./web/bundles/AppBundle/sass/**/*.scss', ['sass'])
+        .on('change', onChange);
+});
