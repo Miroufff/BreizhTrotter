909427385c3de11a4a5955a2dfc786eae37b6f95
jerome.auffret <jerome.auffret.ja@gmail.com>
2016-02-19 11:55:39 +0100

    Fix idexcontroller name

--- BreizhTrotter/Gulpfile.js
+++ BreizhTrotter/Gulpfile.js
@@ -3,15 +3,14 @@ var gulp = require('gulp');
 var sass = sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    gulp.src('./web/bundles/AppBundle/sass/master.scss')
+    gulp.src('./src/AppBundle/sass/master.scss')
         .pipe(sass({sourceComments: 'map'}))
-        .pipe(gulp.dest('./web/css/'));
+        .pipe(gulp.dest('./../../../web/css/'));
 });
 
 gulp.task('watch', function () {
     var onChange = function (event) {
         console.log('File '+event.path+' has been '+event.type);
     };
-    gulp.watch('./src/AppBundle/sass/**/*.scss', ['sass'])
-        .on('change', onChange);
+    gulp.watch('./src/AppBundle/sass/**/*.scss', ['sass']).on('change', onChange);
 });
